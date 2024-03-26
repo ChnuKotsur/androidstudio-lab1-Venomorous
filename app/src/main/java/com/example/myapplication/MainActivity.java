a3cba150932b3e94b7b6f25c3583b141e35ba60f
Venomorous <94299601+Venomorous@users.noreply.github.com>
2024-03-26 19:54:15 +0200

    Add Lab4

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -5,18 +5,17 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import androidx.constraintlayout.widget.ConstraintLayout;
-import androidx.core.content.ContextCompat;
 
-import android.graphics.drawable.GradientDrawable;
+import android.os.Handler;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
-import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.Spinner;
 import android.widget.TextView;
 
 import java.util.Arrays;
+import java.util.Locale;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -26,6 +25,8 @@ public class MainActivity extends AppCompatActivity {
     private TextView hymnTextView;
     private TextView hymnTextTextView;
     private ImageView flagImageView;
+    private int seconds = 0;
+    private boolean running;
 
 
 
@@ -34,8 +35,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Button sendToSecondActivityButton = findViewById(R.id.send_to_second_activity_button);
-        Button shareButton = findViewById(R.id.share_button);
+//        Button sendToSecondActivityButton = findViewById(R.id.send_to_second_activity_button);
+//        Button shareButton = findViewById(R.id.share_button);
 
         countryNameTextView = findViewById(R.id.countryNameTextView);
         hymnTextTextView = findViewById(R.id.hymnTextTextView); //Text of the hymn
@@ -57,7 +58,19 @@ public class MainActivity extends AppCompatActivity {
             public void onNothingSelected(AdapterView<?> parentView) {
             }
         });
-//
+
+        if (savedInstanceState != null) {
+            seconds = savedInstanceState.getInt("seconds");
+            running = savedInstanceState.getBoolean("running");
+        }
+
+        runTimer();
+    }
+    @Override
+    public void onSaveInstanceState(Bundle savedInstanceState) {
+        super.onSaveInstanceState(savedInstanceState);
+        savedInstanceState.putInt("seconds", seconds);
+        savedInstanceState.putBoolean("running", running);
     }
     private void updateUIBasedOnSelectedItem(int selectedItemPosition, ConstraintLayout constraintLayout) {
         // Update TextViews and ImageView based on the selected item
@@ -119,5 +132,38 @@ public class MainActivity extends AppCompatActivity {
         intent.putExtra(Intent.EXTRA_TEXT, selectedHymn + "\n\n" + hymnText);
         intent.setType("text/plain");
         startActivity(intent);
+    }
+
+    public void onClickStart(View view) {
+        running = true;
+    }
+    //Stop the stopwatch running when the Stop button is clicked.
+    public void onClickStop(View view) {
+        running = false;
+    }
+    //Reset the stopwatch when the Reset button is clicked.
+    public void onClickReset(View view) {
+        running = false;
+        seconds = 0;
+    }
+
+    private void runTimer() {
+        final TextView timeView = (TextView)findViewById(R.id.time_view);
+        final Handler handler = new Handler();
+        handler.post(new Runnable() {
+            @Override
+            public void run() {
+                int hours = seconds/3600;
+                int minutes = (seconds%3600)/60;
+                int secs = seconds%60;
+                String time = String.format(Locale.getDefault(),
+                        "%d:%02d:%02d", hours, minutes, secs);
+                timeView.setText(time);
+                if (running) {
+                    seconds++;
+                }
+                handler.postDelayed(this, 1000);
+            }
+        });
     }
 }
